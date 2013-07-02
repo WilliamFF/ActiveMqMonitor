bde17ab6042e5a37469d5c93b8d643b55f30695f
ljchu <aljchu@163.com>
2013-07-02 09:18:27 +0800

    Update and rename matplotlibDraw.py to ActiveMqReportor.py

--- ActiveMqReportor.py
+++ ActiveMqReportor.py
@@ -16,24 +16,6 @@ class report():
     draw report for activemq
     '''
     def __init__(self):
-        configFile=os.getcwd()+'\config.txt'
-        if os.path.exists(configFile):
-            self.configFile=configFile
-        else:
-            configFile=self.getFatherPath(os.getcwd())+'config.txt'
-            if os.path.exists(configFile):
-                self.configFile=configFile
-            else:
-                print('[ERROR 2] No such file or directory:',configFile)
-                return
-        readFile = ReadFile()
-        readFile.setFileName(self.configFile)
-        self.config=dict(readFile.ReadTxtBySpl())
-        self.file=self.config['MonitorFileName']
-        if os.path.exists(self.file)!=1:
-            print('[ERROR 2] No such file or directory:',self.file)
-            return
-        
         self.fontSize=10
         
         self.Content_MessagesDequeued=[]
@@ -43,19 +25,22 @@ class report():
         self.difference_Dequeued=[]
         self.difference_Enqueued=[]
         
-        self.getMessageList()
+        configFile=os.getcwd()+'\config.txt'
+        
+        if os.path.exists(configFile):
+            self.configFile=configFile
+        else:
+            print('[ERROR 2] No such file or directory:',configFile)
+            return
+            
+        readFile = ReadFile()
+        readFile.setFileName(self.configFile)
+        self.config=dict(readFile.ReadTxtBySpl())
+        self.file=self.config['MonitorFileName']
+        if os.path.exists(self.file)!=1:
+            print('[ERROR 2] No such file or directory:',self.file)
+            return
         self.creatReport()
-    
-    def getFatherPath(self,c_path):
-        '''
-        @return: the father directory of thr input path
-        '''
-        b=c_path.split('\\')
-        f_path=''
-        for i in range(0,len(b)-1):
-            c=b[i]+'\\'
-            f_path+=c
-        return f_path
     
     def setPoint(self,a_list):
         '''@return: a list with x and y Coordinate
@@ -106,21 +91,33 @@ class report():
                 for a_line in a_file:
                     line_no +=1
                     a_Content = a_line.rstrip().split('\t')
-                    if a_Content[4].find('MessagesDequeued')==-1:
-                        self.Content_NumberOfPendingMessages.append(a_Content[1])
-                        self.Content_NumberOfConsumers.append(a_Content[2])
-                        self.Content_MessagesEnqueued.append(a_Content[3])
-                        self.Content_MessagesDequeued.append(a_Content[4])
+                    D_Content = a_Content[4]
+                    if D_Content.find('MessagesDequeued')==-1:
+                        if D_Content== 'TimeOut'or D_Content == 'None'and len(self.Content_MessagesDequeued)!=0:
+                            self.Content_NumberOfPendingMessages.append(self.Content_NumberOfPendingMessages[-1])
+                            self.Content_NumberOfConsumers.append(self.Content_NumberOfConsumers[-1])
+                            self.Content_MessagesEnqueued.append(self.Content_MessagesEnqueued[-1])
+                            self.Content_MessagesDequeued.append(self.Content_MessagesDequeued[-1])
+                        else:
+                            self.Content_NumberOfPendingMessages.append(a_Content[1])
+                            self.Content_NumberOfConsumers.append(a_Content[2])
+                            self.Content_MessagesEnqueued.append(a_Content[3])
+                            self.Content_MessagesDequeued.append(a_Content[4])
                     else:
                         line_no +=1
         except Exception as e:
             print(e)
             return
-
-        for j in range(1,len(self.Content_MessagesDequeued)):
-            self.difference_Dequeued.append(int(self.Content_MessagesDequeued[j])-int(self.Content_MessagesDequeued[j-1]))
-            self.difference_Enqueued.append(int(self.Content_MessagesEnqueued[j])-int(self.Content_MessagesEnqueued[j-1]))
-            
+        
+        
+    def getdifference(self):
+            for j in range(1,len(self.Content_MessagesDequeued)):
+                if self.Content_MessagesDequeued[j-1]=='None' or self.Content_MessagesDequeued[j-1]=='TimeOut':
+                    self.difference_Dequeued.append(0)
+                    self.difference_Enqueued.append(0)
+                else:
+                    self.difference_Enqueued.append(int(self.Content_MessagesEnqueued[j])-int(self.Content_MessagesEnqueued[j-1]))
+                    self.difference_Dequeued.append(int(self.Content_MessagesDequeued[j])-int(self.Content_MessagesDequeued[j-1]))
     
     def draw_one_plot(self,x,y,color,Label):
         '''
@@ -145,13 +142,25 @@ class report():
         p1_legend=pl.legend(loc=2,fancybox=True,prop=matplotlib.font_manager.FontProperties(size=self.fontSize))
         p1_legend.get_frame().set_alpha(0.3)
             
+    def getMonitorTime(self):
+        time=self.config['IntervalTime']
+        IntervalType=self.config['IntervalType']
+        if IntervalType=='BySecs':
+            self.time=time+' seconds'
+        elif IntervalType=='ByMins':
+            self.time=time+' minutes'
+    
     def creatReport(self):
-        self.time=self.config['IntervalTime']
+        self.getMonitorTime()
+        self.getMessageList()
+        self.getdifference()
+              
         pl.figure(1)
         ax1 = pl.subplot(311)
         ax2 = pl.subplot(312)
         ax3 = pl.subplot(313)
     
+        #draw Messages Enqueued and Dequeued
         pl.sca(ax1)
         pl.title('Messages Enqueued and Dequeued')
         pl.ylabel('Number')
@@ -166,8 +175,9 @@ class report():
             x12,y12=self.setPoint(self.Content_MessagesEnqueued[1:])
             self.draw_two_plot(x11, y11,'r','$Dequeued$',x12,y12,'black','$Enqueued$')
     
+        #draw Difference
         pl.sca(ax2)
-        pl.title('Messages Difference (per '+self.time+' seconds)')
+        pl.title('Messages Difference (per '+self.time+' )')
         pl.ylabel('Number')
         if self.config['ReportMode']=='D':
             x21,y21=self.setPoint(self.difference_Dequeued)
@@ -194,11 +204,11 @@ class report():
                 average_Enqueued=self.noZeroAverageCalculator(y22)
             self.draw_two_plot(x21, y21,'r','$Dequeued Difference-AVG:'+str(average_Dequeued)+'$',
                        x22,y22,'black','$Enqueued Difference-AVG:'+str(average_Enqueued)+'$')
-    
+        #draw the Number Of Consumers
         pl.sca(ax3)
         pl.title('Number Of Consumers')
         pl.ylabel('Number')
-        pl.xlabel('Times(1 equals '+self.time+' seconds)')
+        pl.xlabel('Times(1 equals '+self.time+' )')
         x3,y3=self.setPoint(self.Content_NumberOfConsumers[1:])
         self.draw_one_plot(x3, y3,'green','$Consumers$')
 
